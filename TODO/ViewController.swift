cd40702ee52e060836a61d86eea3bfea2c5029d2
Grover Chen <groverchen1994@gmail.com>
2017-03-18 22:54:57 -0400

    Add filter

--- TODO/ViewController.swift
+++ TODO/ViewController.swift
@@ -10,39 +10,49 @@ import UIKit
 import RealmSwift
 
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchDisplayDelegate {
-
-    let realm = try! Realm()
-    
-    var todoList: Results<TodoItem>!
-    var filteredTodoList: Results<TodoItem>!
-    
-    var currentCreateAction:UIAlertAction!
-    
-    override func viewWillAppear(_ animated: Bool) {
-        readTasksAndUpdateUI()
-    }
-    
-    func readTasksAndUpdateUI(){
-        todoList = realm.objects(TodoItem.self)
-        self.tableView.setEditing(false, animated: true)
-        self.tableView.reloadData()
-    }
-    
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    enum SortType: String {
+        case detail = "detail"
+        case createdAt = "createdAt"
+        case status = "status"
+    }
+    enum SortDirection {
+        case asc
+        case desc
+    }
+    
+    var currentCreateAction:UIAlertAction!
+    var searchQuery = ""
+    var sortType = SortType.detail
+    var sortDirection = SortDirection.asc
+
+    let realm = try! Realm()
+    var todoList: Results<TodoItem> {
+        get {
+            // All Objects
+            var objects = realm.objects(TodoItem.self)
+            
+            // Add Search Query
+            if (!searchQuery.isEmpty) {
+                objects = objects.filter("detail CONTAINS '\(searchQuery.lowercased())'")
+            }
+            // Add Sorting
+            var ascending = false
+            if sortDirection == .asc {
+                ascending = true
+            }
+            objects = objects.sorted(byKeyPath: sortType.rawValue, ascending: ascending)
+            return objects
+        }
+    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
-        var item = todoList[indexPath.row]
-        
-        if tableView == searchDisplayController?.searchResultsTableView {
-//            if filteredTodoList != nil{
-                item = filteredTodoList[indexPath.row]
-//            }
-        }
+        let item = todoList[indexPath.row]
         
         cell.textLabel!.text = item.detail
         cell.detailTextLabel!.text = "\(item.status)"
@@ -50,117 +60,68 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
-    // change priority of each row
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // Change priority of each row
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        var item = todoList[indexPath.row]
-        
-        if tableView == searchDisplayController?.searchResultsTableView {
-            item = filteredTodoList[indexPath.row]
-        }
+        let item = todoList[indexPath.row]
 
         try! self.realm.write({
             item.status += 1
         })
         
-        tableView.reloadRows(at: [indexPath], with: .automatic)
+        tableView.reloadData()
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("todoList: " + String(todoList.count))
+        return todoList.count
+    }
+    
     
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
-        if tableView == searchDisplayController?.searchResultsTableView {
-            if filteredTodoList == nil
-            {
-                return 0
-            }
-            else
-            {
-                print ("filtseredTodoList" + String(filteredTodoList.count))
-                return filteredTodoList.count
-            }
-        }
-        else {
-            print("todoList: " + String(todoList.count))
-            return todoList.count
-        }
-        return 0
-    }
-    
+
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
         
         let deleteAction = UITableViewRowAction(style: .default, title: "Delete") { (deleteAction, indexPath) -> Void in
             
             //Deletion
-            var listToBeDeleted = self.todoList[indexPath.row]
-            
-            if tableView == self.searchDisplayController?.searchResultsTableView {
-                listToBeDeleted = self.filteredTodoList[indexPath.row]
-            }
+            let taskToBeDeleted = self.todoList[indexPath.row]
             
             try! self.realm.write{
-                self.realm.delete(listToBeDeleted)
-                self.readTasksAndUpdateUI()
+                self.realm.delete(taskToBeDeleted)
             }
+            self.tableView.reloadData()
         }
         
         let editAction = UITableViewRowAction(style: UITableViewRowActionStyle.normal, title: "Edit") { (editAction, indexPath) -> Void in
             
             // Edit
-            var listToBeUpdated = self.todoList[indexPath.row]
-            
-            if tableView == self.searchDisplayController?.searchResultsTableView {
-                listToBeUpdated = self.filteredTodoList[indexPath.row]
-            }
-            
-            self.displayAlertToAddTaskList(listToBeUpdated)
-            
+            let taskToBeUpdated = self.todoList[indexPath.row]
+            self.displayAlertToUpdateTask(taskToBeUpdated)
         }
         return [deleteAction, editAction]
     }
     
-    
-    func searchDisplayController(_ controller: UISearchDisplayController, shouldReloadTableForSearch searchString: String?) -> Bool {
-        
-        
-//        let searchPredicate = NSPredicate(format: "detail CONTAINS[c] %@", searchString!)
-//        let searchPredicate = NSPredicate()
-        
-//        filteredTodoList = realm.objects(TodoItem.self).filter(searchPredicate)
-        
-
-        var todos = realm.objects(TodoItem.self)
-        todos = todos.filter("detail CONTAINS '\(searchString)'")
-        
-//        objects = objects.filter("text CONTAINS '\(searchQuery)'")
-        
-        print("searchDisplayController\n")
-
-        return true
-    }
-    
-
-    func displayAlertToAddTaskList(_ updatedList:TodoItem!){
+    func displayAlertToUpdateTask(_ updatedTask:TodoItem!){
         let title = "Update Tasks List"
         let doneTitle = "Update"
         
-        let alertController = UIAlertController(title: title, message: "Write the name of your tasks list.", preferredStyle: UIAlertControllerStyle.alert)
+        let alertController = UIAlertController(title: title, message: "Update task", preferredStyle: UIAlertControllerStyle.alert)
         
         let createAction = UIAlertAction(title: doneTitle, style: UIAlertActionStyle.default) { (action) -> Void in
-            
-            let listName = alertController.textFields?.first?.text
-            
-            // update mode
+            let taskName = alertController.textFields?.first?.text
+            // update task
             try! self.realm.write{
-                updatedList.detail = listName!
-                updatedList.createdAt = NSDate()
-                self.readTasksAndUpdateUI()
+                updatedTask.detail = taskName!
+                updatedTask.createdAt = NSDate()
+                self.tableView.reloadData()
             }
         }
         
@@ -170,16 +131,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         alertController.addTextField { (textField) -> Void in
             textField.placeholder = "Task List Name"
-            textField.addTarget(self, action: #selector(ViewController.listNameFieldDidChange(_:)), for: UIControlEvents.editingChanged)
-            if updatedList != nil{
-                textField.text = updatedList.detail
+            textField.addTarget(self, action: #selector(ViewController.taskNameFieldDidChange(_:)), for: UIControlEvents.editingChanged)
+            if updatedTask != nil{
+                textField.text = updatedTask.detail
             }
         }
         
         self.present(alertController, animated: true, completion: nil)
     }
 
-    func listNameFieldDidChange(_ textField:UITextField){
+    func taskNameFieldDidChange(_ textField:UITextField){
         self.currentCreateAction.isEnabled = (textField.text?.characters.count)! > 0
     }
 
@@ -190,7 +151,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         alertController.addTextField {
             (UITextField) in
         }
-        
+
         let action_cancel = UIAlertAction.init(title: "Cancel", style: .cancel) {
             (UIAlertAction) -> Void in
         }
@@ -210,27 +171,28 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             
             try! self.realm.write({
                 self.realm.add(todoItem)
-//                self.tableView.insertRows(at: [IndexPath.init(row: self.todoList.count-1, section: 0)], with: .automatic)
-                self.readTasksAndUpdateUI()
+                self.tableView.reloadData()
             })
         }
         
         alertController.addAction(action_add)
         present(alertController, animated: true, completion: nil)
     }
-
     
     @IBAction func didSelectSortCriteria(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == 0{
             // A-Z
-            self.todoList = self.todoList.sorted(byKeyPath: "detail")
+            sortType = .detail
+            sortDirection = .asc
         }
         else if sender.selectedSegmentIndex == 1 {
             // date
-            self.todoList = self.todoList.sorted(byKeyPath: "createdAt", ascending:false)
+            sortType = .createdAt
+            sortDirection = .desc
         }
         else {
-            self.todoList = self.todoList.sorted(byKeyPath: "status", ascending:false)
+            sortType = .status
+            sortDirection = .desc
         }
         self.tableView.reloadData()
     }
@@ -244,6 +206,26 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print ("loading....")
+        tableView.reloadData()
+        print ("loading completed")
+    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchQuery = searchText
+        tableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
     }
 
 
